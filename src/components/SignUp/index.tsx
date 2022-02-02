64b4702d90f0d924e0e73c3a24236cee834ea4ae
duck <godqhr2256@gmail.com>
2022-02-02 18:22:18 +0900

    [#6] 디자인 작업 마무리 & 모달 작업 ~ing

--- src/components/SignUp/index.tsx
+++ src/components/SignUp/index.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useCallback } from 'react';
 import styled from '@emotion/styled';
 import { lightTheme } from 'src/theme';
+import useInput from 'src/hooks/useInput';
+import Modal from 'react-modal';
 
 const Background = styled.div`
   background-color: #f4f4fc;
@@ -31,22 +33,66 @@ const Div = styled.div`
 const SignUpLabel = styled.label`
   font-size: 23px;
 `;
-const NickSubmit = styled.input``;
+const NickInput = styled.input`
+  width: 510px;
+  height: 30px;
+  border: none;
+  border-bottom: 1px solid #ccc;
+  font-size: 14px;
+`;
 
-const NickLabel = styled.label``;
+const Label = styled.label`
+  font-size: 16px;
+  display: flex;
+`;
+const ValidationLabel = styled.label`
+  font-size: 14px;
+  float: left;
+  color: #ff0a0a;
+`;
+const EmailInput = styled.input`
+  width: 510px;
+  height: 30px;
+  border: none;
+  border-bottom: 1px solid #ccc;
+  font-size: 14px;
+`;
 
-const EmailSubmit = styled.input``;
+const EmailBtn = styled.button`
+  width: 70px;
+  height: 35px;
+  float: right;
+  border-radius: 6px;
+  background-color: #1c00ff;
+  color: white;
+  border-style: none;
+  cursor: pointer;
+`;
+const EmailDiv = styled.div`
+  display: flex;
+  width: 510px;
+`;
+const LectureModal = styled.input`
+  width: 510px;
+  height: 30px;
+  border: none;
+  border-bottom: 1px solid #ccc;
+  font-size: 14px;
+  background-color: white;
+`;
 
-const EmailLabel = styled.label``;
-
-const EmailBtn = styled.button``;
-const LectureModal = styled.div``;
-
-const FavoriteModal = styled.div``;
+const FavoriteModal = styled.input`
+  width: 510px;
+  height: 30px;
+  border: none;
+  border-bottom: 1px solid #ccc;
+  font-size: 14px;
+  background-color: white;
+`;
 
 const RegisterBtn = styled.button`
   cursor: pointer;
-  width: 509px;
+  width: 510px;
   height: 56px;
   color: white;
   border-style: none;
@@ -56,34 +102,99 @@ const RegisterBtn = styled.button`
 `;
 
 const Register = () => {
+  // const [nick, onChangeNick] = useInput('');
+  // const [email, onChangeEmail] = useInput('');
+  const [nick, setNick] = useState('');
+  const onChangeNick = useCallback((e) => {
+    setNick(e.target.value);
+  }, []);
+
+  const [email, setEmail] = useState('');
+  const onChangeEmail = useCallback((e) => {
+    setEmail(e.target.value);
+  }, []);
+  const emailCheck = (e) => {
+    const regExp = /\d{8}@\w[bu]+\.\w[ac]+\.\w[kr]/i;
+    // 이메일 형식 : 숫자 8개@bu.ac.kr
+    console.log('이메일 유효성 검사 :: ', regExp.test(e.target.value));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const [modalLecture, setModalLecture] = useState(false);
+  const modalClose = (e) => {
+    setModalLecture(false);
+  };
+  const modalOpen = (e) => {
+    setModalLecture(true);
+    return (
+      <>
+        <Modal />
+      </>
+    );
+  };
 
   return (
-  <>
-    <Background>
-      <SignUpForm>
-        <Div>
-          <SignUpLabel>회원가입</SignUpLabel>
-          <div>
-            <NickLabel htmlFor="user-nick">닉네임</NickLabel>
-            <NickSubmit />
-          </div>
-          <div>
-            <EmailLabel htmlFor="user-email">이메일</EmailLabel>
-            <EmailSubmit />
-            <EmailBtn>인증</EmailBtn>
-          </div>
-          <div>
-            <LectureModal />
-          </div>
-          <div>
-            <FavoriteModal />
-          </div>
-          <RegisterBtn>가입하기</RegisterBtn>
-        </Div>
-      </SignUpForm>
-    </Background>
-  </>
-  )
+    <>
+      <Background>
+        <SignUpForm>
+          <Div>
+            <SignUpLabel>회원가입</SignUpLabel>
+            <br /> <br /> <br /> <br /> <br />
+            <div>
+              <Label htmlFor="user-nick">닉네임</Label>
+              <br />
+              <NickInput
+                name="user-nick"
+                required
+                onChange={onChangeNick}
+                value={nick}
+                placeholder="닉네임을 입력해주세요."
+              />
+            </div>
+            <br /> <br /> <br />
+            <div>
+              <Label htmlFor="user-email">이메일</Label>
+              <br />
+              <EmailDiv>
+                <EmailInput
+                  name="user-email"
+                  required
+                  onChange={onChangeEmail}
+                  value={email}
+                  placeholder="인증메일이 발송되니 정확히 입력해주세요."
+                  onBlur={emailCheck}
+                />
+                <EmailBtn>인증</EmailBtn>
+              </EmailDiv>
+              <br />
+              <ValidationLabel>ㄴ 대학교 웹메일 계정만 웹메일이 발송 가능합니다.</ValidationLabel>
+              <br />
+              <ValidationLabel>ㄴ 형식: 학번@bu.ac.kr</ValidationLabel>
+            </div>
+            <br /> <br /> <br />
+            <div>
+              <Label htmlFor="user-lecture">전공 선택</Label>
+              <LectureModal type="button" onClick={modalOpen} placeholder="최대 2개까지 선택할 수 있습니다.">
+                {/* // eslint-disable-next-line react/jsx-boolean-value */}
+              </LectureModal>
+            </div>
+            <br /> <br /> <br />
+            <div>
+              <Label htmlFor="user-favorite">관심사 선택</Label>
+              <FavoriteModal placeholder="#팀플 없음 #학점 잘줌" />
+            </div>
+            <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
+            <RegisterBtn type="submit" onSubmit={onSubmit}>
+              가입하기
+            </RegisterBtn>
+          </Div>
+        </SignUpForm>
+      </Background>
+    </>
+  );
 };
 
 export default Register;
